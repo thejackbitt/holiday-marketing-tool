edb5491f6abc407e4303684587983d00c7fa6876
helm0127 <helm0127@morris.umn.edu>
2024-01-09 13:46:41 -0600

    committing to be able to pull

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+//this script is for the homepage
+//TODO
+//make listener for submit button taking input field value
+//dynamically generated html?
+//access local storage for saved items - String literals for jquery
+
