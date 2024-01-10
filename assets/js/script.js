93b30bb042803f762cf3e9816002e8d9f3b08a0f
helm0127 <helm0127@morris.umn.edu>
2024-01-10 10:17:37 -0600

    added more todos

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
 //TODO
+//dayjs date in header
 //make script to send user to next page upon button click
+//learn more button at bottom send to wikipedia page
 //save project data in local storage
 //recall project data and display to page
 //data will likely be saved as search parameters so this script will need an api call
