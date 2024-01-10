15f7591eb794238332b8da4c30ec97002c1f12f1
helm0127 <helm0127@morris.umn.edu>
2024-01-10 10:13:50 -0600

    added some more js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,7 @@
 //TODO
 //make script to send user to next page upon button click
-//
+//save project data in local storage
+//recall project data and display to page
+//data will likely be saved as search parameters so this script will need an api call
+//and saved data will be used to recall the data from fetch
+
