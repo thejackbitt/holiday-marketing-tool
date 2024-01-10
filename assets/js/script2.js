0f127d83f77cc55807dfde3dbbeb0b62e6d58d72
Dan Noonan <dnoonan23@yahoo.com>
2024-01-10 12:24:53 -0600

    trying to figure out setting start and end dates

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -6,36 +6,49 @@
 //button listener
 //
 
-const btn1 = $('#btn1');
-const input1 = $('input1');
-git config pull.rebase true
+// const btn1 = $('#btn1');
+// const input1 = $('input1');
+// git config pull.rebase true
 
-btn1.on('click' function() {
+// btn1.on('click' function() {
 
+// });
+
+// function handleSearchFormSubmit(event) {
+//   event.preventDefault();
+
+//   var searchInputVal = document.querySelector('#search-input').value;
+//   var formatInputVal = document.querySelector('#format-input').value;
+
+//   if (!searchInputVal) {
+//     console.error('You need a search input value!');
+//     return;
+//   }
+
+//   // Build the url for sending the user to the second web page
+//   var queryString = './search-results.html?q=' + searchInputVal + '&format=' + formatInputVal;
+
+//   // send user to another page
+//   console.log(queryString)
+//   location.assign(queryString);
+
+//   console.log("ok")
+// }
+
+// searchFormEl.addEventListener('submit', handleSearchFormSubmit);
+// ;
+var startDate = document.querySelector("#start");
+var endDate = document.getElementById("#end");
+
+
+let field = document.querySelector('#date');
+
+// Handle date changes
+date.addEventListener('input', function () {
+
+  // Get the date
+  let date = new Date(field.value);
+  console.log(date);
+  
 });
-
-function handleSearchFormSubmit(event) {
-  event.preventDefault();
-
-  var searchInputVal = document.querySelector('#search-input').value;
-  var formatInputVal = document.querySelector('#format-input').value;
-
-  if (!searchInputVal) {
-    console.error('You need a search input value!');
-    return;
-  }
-
-  // Build the url for sending the user to the second web page
-  var queryString = './search-results.html?q=' + searchInputVal + '&format=' + formatInputVal;
-
-  // send user to another page
-  console.log(queryString)
-  location.assign(queryString);
-
-  console.log("ok")
-}
-
-searchFormEl.addEventListener('submit', handleSearchFormSubmit);
-;
-
 
