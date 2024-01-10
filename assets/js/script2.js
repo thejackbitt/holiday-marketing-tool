21b48acf6a88821631888e6750c9d992f88ca21f
helm0127 <helm0127@morris.umn.edu>
2024-01-10 11:15:15 -0600

    added more js

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -1,9 +1,6 @@
 //FOR PROJECT.HTML
 
 //TODO
-//input field into api fetch address with parameters
-//api address builder
-//button listener
 //
 
 const btn1 = $('#btn1');
@@ -39,3 +36,114 @@ searchFormEl.addEventListener('submit', handleSearchFormSubmit);
 ;
 
 
+
+Andorra	AD
+Albania	AL
+Argentina	AR
+Austria	AT
+Australia	AU
+Åland Islands	AX
+Bosnia and Herzegovina	BA
+Barbados	BB
+Belgium	BE
+Bulgaria	BG
+Benin	BJ
+Bolivia	BO
+Brazil	BR
+Bahamas	BS
+Botswana	BW
+Belarus	BY
+Belize	BZ
+Canada	CA
+Switzerland	CH
+Chile	CL
+China	CN
+Colombia	CO
+Costa Rica	CR
+Cuba	CU
+Cyprus	CY
+Czechia	CZ
+Germany	DE
+Denmark	DK
+Dominican Republic	DO
+Ecuador	EC
+Estonia	EE
+Egypt	EG
+Spain	ES
+Finland	FI
+Faroe Islands	FO
+France	FR
+Gabon	GA
+United Kingdom	GB
+Grenada	GD
+Guernsey	GG
+Gibraltar	GI
+Greenland	GL
+Gambia	GM
+Greece	GR
+Guatemala	GT
+Guyana	GY
+Honduras	HN
+Croatia	HR
+Haiti	HT
+Hungary	HU
+Indonesia	ID
+Ireland	IE
+Isle of Man	IM
+Iceland	IS
+Italy	IT
+Jersey	JE
+Jamaica	JM
+Japan	JP
+South Korea	KR
+Liechtenstein	LI
+Lesotho	LS
+Lithuania	LT
+Luxembourg	LU
+Latvia	LV
+Morocco	MA
+Monaco	MC
+Moldova	MD
+Montenegro	ME
+Madagascar	MG
+North Macedonia	MK
+Mongolia	MN
+Montserrat	MS
+Malta	MT
+Mexico	MX
+Mozambique	MZ
+Namibia	NA
+Niger	NE
+Nigeria	NG
+Nicaragua	NI
+Netherlands	NL
+Norway	NO
+New Zealand	NZ
+Panama	PA
+Peru	PE
+Papua New Guinea	PG
+Poland	PL
+Puerto Rico	PR
+Portugal	PT
+Paraguay	PY
+Romania	RO
+Serbia	RS
+Russia	RU
+Sweden	SE
+Singapore	SG
+Slovenia	SI
+Svalbard and Jan Mayen	SJ
+Slovakia	SK
+San Marino	SM
+Suriname	SR
+El Salvador	SV
+Tunisia	TN
+Turkey	TR
+Ukraine	UA
+United States	US
+Uruguay	UY
+Vatican City	VA
+Venezuela	VE
+Vietnam	VN
+South Africa	ZA
+Zimbabwe	ZW
