0299e1129e374e5fd821ec829a15163753f3436d
Dan Noonan <dnoonan23@yahoo.com>
2024-01-12 10:49:48 -0600

    merge

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -537,6 +537,7 @@ function getApi() {
     })
 };
 
+
 //Calendar
 today = new Date();
 currentMonth = today.getMonth();
@@ -547,7 +548,7 @@ selectMonth = document.getElementById("month");
 months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
 monthAndYear = document.getElementById("monthAndYear");
-function showCalendar(startDate, endDate) {
+function showCalendar(month, year) {
   monthAndYear = document.getElementById("monthAndYear");
   showCalendar(currentMonth, currentYear);
 
@@ -632,23 +633,6 @@ function showCalendar(startDate, endDate) {
   
     }
 
-function next() {
-  currentYear = (currentMonth === 11) ? currentYear + 1 : currentYear;
-  currentMonth = (currentMonth + 1) % 12;
-  showCalendar(currentMonth, currentYear);
-}
-
-function previous() {
-  currentYear = (currentMonth === 0) ? currentYear - 1 : currentYear;
-  currentMonth = (currentMonth === 0) ? 11 : currentMonth - 1;
-  showCalendar(currentMonth, currentYear);
-}
-
-function jump() {
-  currentYear = parseInt(selectYear.value);
-  currentMonth = parseInt(selectMonth.value);
-  showCalendar(currentMonth, currentYear);
-}
 
 function daysInMonth(iMonth, iYear) {
   return 32 - new Date(iYear, iMonth, 32).getDate();
@@ -704,5 +688,4 @@ function retrieveSavedData() {
 
 
 
-
-
+}
