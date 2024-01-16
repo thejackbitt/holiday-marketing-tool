7a6ccc7e9f4764711055c633c8498ba58e9665e2
EcoSiege <153530802+EcoSiege@users.noreply.github.com>
2024-01-16 12:48:09 -0600

    redoing api logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,6 +9,7 @@
 //and saved data will be used to recall the data from fetch
 
 const now = dayjs();
+const requestUrl = 'https://date.nager.at/api/v3/NextPublicHolidaysWorldwide';
 const currentDateDisp = $('#current-date');
 const newProject = $('#new-project');
 const learnMore = $('#learn-more');
@@ -19,6 +20,7 @@ const countyNameArray = [];
 var country;
 let j = 0;
 let h = 0;
+let count = 0;
 
 // retrieving the data from local storage
 const savedProjectData = JSON.parse(localStorage.getItem('projectData')) || {};
@@ -37,18 +39,17 @@ learnMore.on('click', function () {
    var holidayName = $('#holiday-OTD');
    var urlBuild = 'en.wikipedia.org/wiki/' + holidayName;
    window.open(urlBuild, '_blank');
-})
+});
 
 //this function is to create and populate the holiday of the day carousel on the home page
 function getApi() {
    //nager api call
-   var requestUrl = 'https://date.nager.at/api/v3/NextPublicHolidaysWorldwide';
+
    fetch(requestUrl)
       .then(function (response) {
          return response.json();
       })
       .then(function (data) {
-         let count = 0;
          for (var i = 0; i < data.length; i++) {
 
             const nagerData = data;
@@ -71,69 +72,79 @@ function getApi() {
                origin: '*',
             });
 
+            getWiki()
+
             // wikipedia api call
-            fetch(`${url}?${params}`)
-               .then(function (response) { return response.json(); })
-               .then(function (data) {
-                  let resultsArray = data.query.search;
-                  finalData[j] = { ...resultsArray[j], ...nagerData[h] };
-                  finalData[j].country = countyNameArray[h].name;
-                  h++;
-                  return (finalData);
-               })
-               .then(function (item) {
-                  // this function is supposed to take the info from the wiki api call and fill the information into a display element
-                     // creating elements to display the information
-                  var carouselItem = document.createElement('div');
-                  var table = document.createElement('table');
-                  var tableBody = document.createElement('tbody');
-                  var createTableRow = document.createElement('tr');
-                  var tableData = document.createElement('td');
-                  var holidayHeader = document.createElement('h2');
-                  var holidayOrigin = document.createElement('h3');
-                  var wikiInfo = document.createElement('div');
+            async function getWiki() {
+               const response = await fetch(`${url}?${params}`)
+               const wikiData = await response.json();
+               console.log(wikiData.query.search)
+               }
+         //          .then(function (data) {
+         //             let resultsArray = data.query.search;
+         //             console.log('results array ' + j)
+         //             console.log(resultsArray[j])
+         //             finalData[j] = { ...resultsArray[j], ...nagerData[h] };
+         //             finalData[j].country = countyNameArray[h].name;
+         //             h++;
+         //             return (finalData);
+         //          })
+         //          .then(function (item) {
+         //             // this function takes the info from the wiki api call and puts the information into a display element
+         //             // creating elements to display the information
+         //             var carouselItem = document.createElement('div');
+         //             var table = document.createElement('table');
+         //             var tableBody = document.createElement('tbody');
+         //             var createTableRow = document.createElement('tr');
+         //             var tableData = document.createElement('td');
+         //             var holidayHeader = document.createElement('h2');
+         //             var holidayOrigin = document.createElement('h3');
+         //             var wikiInfo = document.createElement('div');
+         //             console.log(item[0])
 
-                     // text body and link made from the wiki api data
-                  var itemTitle = item[0].title;
-                  var itemSnippet = item[0].snippet;
-                  var itemUrl = encodeURI(`https://en.wikipedia.org/wiki/${item[0].title}`);
+         //             // text body and link made from the wiki api data
+         //             var itemTitle = item[0].title;
+         //             var itemSnippet = item[0].snippet;
+         //             var itemUrl = encodeURI(`https://en.wikipedia.org/wiki/${item[0].title}`);
 
-                  wikiInfo.innerHTML = `<div class="resultItem">
-                           <h3 class="resultTitle">
-                           <a href="${itemUrl}" target="_blank" rel="noopener">${itemTitle}</a>
-                           </h3>
-                           <p class="resultSnippet"><a href="${itemUrl}"  target="_blank" rel="noopener">
-                           ${itemSnippet}</a></p>
-                           </div>`;
+         //             wikiInfo.innerHTML = `<div class="resultItem">
+         //                <h3 class="resultTitle">
+         //                <a href="${itemUrl}" target="_blank" rel="noopener">${itemTitle}</a>
+         //                </h3>
+         //                <p class="resultSnippet"><a href="${itemUrl}"  target="_blank" rel="noopener">
+         //                ${itemSnippet}</a></p>
+         //                </div>`;
 
-                  // creating the carousel cards
-                  table.setAttribute('class', 'd-block w-100');
-                  if (count === 0) {
-                     carouselItem.setAttribute('class', 'carousel-item active');
-                  } else {
-                     carouselItem.setAttribute('class', 'carousel-item');
-                  };
+         //             // creating the carousel cards
+         //             table.setAttribute('class', 'd-block w-100');
+         //             if (count === 0) {
+         //                carouselItem.setAttribute('class', 'carousel-item active');
+         //             } else {
+         //                carouselItem.setAttribute('class', 'carousel-item');
+         //             };
 
-                  // putting the display items into the html
-                  holidayHeader.textContent = item[0].name;
-                  holidayOrigin.textContent = item[0].country;
+         //             // putting the display items into the html
+         //             holidayHeader.textContent = item[0].name;
+         //             holidayOrigin.textContent = item[0].country;
 
-                  tableData.appendChild(holidayHeader);
-                  tableData.appendChild(holidayOrigin);
-                  tableData.appendChild(wikiInfo);
+         //             tableData.appendChild(holidayHeader);
+         //             tableData.appendChild(holidayOrigin);
+         //             tableData.appendChild(wikiInfo);
 
-                  createTableRow.appendChild(tableData);
-                  tableBody.appendChild(createTableRow);
-                  table.appendChild(tableBody);
-                  carouselItem.appendChild(table);
-                  tableCard.append(carouselItem);
-                  count++;
-               })
-               .catch(function (error) { console.log(error); });
+         //             createTableRow.appendChild(tableData);
+         //             tableBody.appendChild(createTableRow);
+         //             table.appendChild(tableBody);
+         //             carouselItem.appendChild(table);
+         //             tableCard.append(carouselItem);
+         //             count++;
+         //          })
+         //          .catch(function (error) { console.log(error); });
+         //    }
 
-            if (i >= 4) return;
-         }
-      })
+         //    if (i >= 4) return;
+         // }
+      }
 }
+      )}
 
 getApi();
